ebce6240590d3657554c59fd5a68db81562f9b4b
Wellington Gallo <wellinton.gallo77@gmail.com>
2021-05-19 09:58:09 -0300

    refreshToken

--- services/api.ts
+++ services/api.ts
@@ -1,11 +1,72 @@
-import axios from 'axios';
-import { parseCookies } from 'nookies';
+import axios, { AxiosError } from 'axios';
+import { parseCookies, setCookie } from 'nookies';
 
-const cookies = parseCookies();
+let cookies = parseCookies();
+let isRefreshing = false;
+let failedRequestQueue = [];
 
 export const api = axios.create({
   baseURL: 'http://localhost:3333',
   headers: {
     Authorization: `Bearer ${cookies['nextauth.token']}`
   }
-})
+});
+
+api.interceptors.response.use(response => {
+  return response;
+}, (error: AxiosError) => {
+  if (error.response.status === 401) {
+    if (error.response.data?.code === 'token.expired') {
+      cookies = parseCookies();
+
+      const { 'nextauth.refreshToken': refreshToken } = cookies;
+      const originalConfig = error.config;
+
+      if (!isRefreshing) {
+        isRefreshing = true;
+
+        api.post('/refresh', {
+          refreshToken
+        }).then(response => {
+          const { token } = response.data;
+  
+          setCookie(undefined, 'nextauth.token', token, {
+            maxAge: 60 * 60 * 24 * 30,
+            path: '/'
+          });
+    
+          setCookie(undefined, 'nextauth.refreshToken', response.data.refreshToken, {
+            maxAge: 60 * 60 * 24 * 30,
+            path: '/'
+          });
+  
+          api.defaults.headers['Authorization'] = `Bearer ${token}`;
+
+          failedRequestQueue.forEach(request => request.resolve(token));
+          failedRequestQueue = [];
+        }).catch(err => {
+          failedRequestQueue.forEach(request => request.reject(err));
+          failedRequestQueue = [];
+        }).finally(() => {
+          isRefreshing = false;
+        });
+      }
+
+      return new Promise((resolve, reject) => {
+        failedRequestQueue.push({
+          resolve: (token: string) => {
+            originalConfig.headers['Authorization'] = `Bearer ${token}`;
+
+            resolve(api(originalConfig));
+          },
+          reject: (error: AxiosError) => {
+            reject(error);
+          },
+        });
+      })
+    } else {
+
+    }
+  }
+  console.log(error.response.status);
+});
