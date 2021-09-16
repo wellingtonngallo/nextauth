9c4e48c138b009ac35fc2948cdb34c79dd256db9
Wellington Gallo <wellinton.gallo77@gmail.com>
2021-09-15 21:46:42 -0300

    feat: brodcastChannel

--- context/AuthContext.tsx
+++ context/AuthContext.tsx
@@ -16,6 +16,7 @@ type SignInCredentials = {
 
 type AuthContextData = {
   signIn(credentials: SignInCredentials): Promise<void>;
+  signOut(): void;
   user: User;
   isAuthenticated: boolean;
 }
@@ -26,9 +27,13 @@ type AuthProviderProps = {
 
 const AuthContext = createContext({} as AuthContextData);
 
+let authChannel: BroadcastChannel;
+
 export function signOut() {
   destroyCookie(undefined, 'nextauth.token');
   destroyCookie(undefined, 'nextauth.refreshToken');
+
+  authChannel.postMessage('signOut');
 
   Router.push('/');
 }
@@ -36,6 +41,23 @@ export function signOut() {
 export function AuthProvider({ children }: AuthProviderProps) {
   const [user, setUser] = useState<User>();
   const isAuthenticated = !!user;
+
+  useEffect(() => {
+    authChannel = new BroadcastChannel('auth');
+
+    authChannel.onmessage = (message) => {
+      switch (message.data) {
+        case 'signOut': 
+          signOut();
+          break;
+        case 'signIn': 
+          Router.push('/dashboard')
+          break;
+        case 'default':
+          break;
+      }
+    }
+  }, []);
 
   useEffect(() => {
     const { 'nextauth.token': token} = parseCookies();
@@ -80,13 +102,14 @@ export function AuthProvider({ children }: AuthProviderProps) {
       api.defaults.headers['Authorization'] = `Bearer ${token}`;
 
       Router.push('/dashboard');
+      authChannel.postMessage('signIn');
     } catch (err) {
       console.log(err);
     }
   }
 
   return (
-    <AuthContext.Provider value={{ signIn, isAuthenticated, user }}>
+    <AuthContext.Provider value={{ signIn, signOut, isAuthenticated, user }}>
       {children}
     </AuthContext.Provider>
   )
--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -3,11 +3,12 @@ import { useAuth } from "../context/AuthContext"
 import { withSSRAuth } from "../utils/withSSRAuth";
 
 export default function Dashboard() {
-  const { user } = useAuth();
+  const { user, signOut } = useAuth();
 
   return (
     <>
       <h1>Dashboard {user?.email}</h1>
+      <button onClick={signOut}>Sign out</button>
       <Can permissions={['metrics.list']}>
         <div>Metricas</div>
       </Can>
